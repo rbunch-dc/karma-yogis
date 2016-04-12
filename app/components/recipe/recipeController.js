146f2c4b54685ba1abeb0702efe9f12872c32643
curtjenk <curtjenk@comcast.net>
2016-04-12 12:38:09 -0400

    Added a missing ">".  Include boilerplate files for Recipes page

---
+++ app/components/recipe/recipeController.js
@@ -0,0 +1,17 @@
+recipeApp.controller('recipeController', function($scope, utilLocalStore, recipesApi) {
+
+var inventory = [];
+function inventoryItem(item, qty, uom){
+  this.item = item;
+  this.qty = qty;
+  this.uom = uom;
+}
+
+inventory.push(new inventoryItem("milk", 1, "gallon"));
+inventory.push(new inventoryItem("bread", 1, "loaf"));
+
+utilLocalStore.setInventory(inventory);
+
+
+
+});
